c92eb42fd5ba2f3ced404fe75a3fa230bda8fba0
kivensolo <760553478@qq.com>
2016-04-05 00:14:56 +0800

    添加本地海报图片xml数据，新增播放器状态图片

--- src/com/AsuyncTaskJsonTest/NewsBean.java
+++ src/com/AsuyncTaskJsonTest/NewsBean.java
@@ -1,9 +1,5 @@
 package com.AsuyncTaskJsonTest;
 
-/**
- * Created by zhi.wang on 2015/11/7.
- * 封装类
- */
 public class NewsBean {
     public String newspictureUrl;
     public String newsTitle;
---
+++ src/com/posterfilmlist/PosterGroupInfo.java
@@ -0,0 +1,22 @@
+package com.posterfilmlist;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Copyright(C) 2015, 北京视达科科技有限公司
+ * All rights reserved.
+ * author: King.Z
+ * date: 2016 2016/4/2 17:06
+ * description:
+ */
+public class PosterGroupInfo implements Serializable {
+
+
+    public String id;
+    public String title;
+    public ArrayList<Poster> poster;
+
+
+    }
+}
