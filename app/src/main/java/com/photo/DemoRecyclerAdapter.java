1e7cb60dc5743ddf50a5b90cde1a050782380e19
kivensolo <760553478@qq.com>
2016-08-07 23:34:37 +0800

    添加RecycleView

---
+++ app/src/main/java/com/photo/DemoRecyclerAdapter.java
@@ -0,0 +1,110 @@
+package com.photo;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kingz.customDemo.R;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
+/**
+ * Copyright(C) 2015, 北京视达科科技有限公司
+ * All rights reserved.
+ * author: King.Z
+ * date:  2016/8/7 22:41
+ * description:
+ *   RecycleView的数据适配器
+ */
+public class DemoRecyclerAdapter extends RecyclerView.Adapter<DemoViewHolder> {
+
+    private List<PhotoDataModel> mDataModels;
+    private List<Integer> mHeights;
+
+    public DemoRecyclerAdapter(List<PhotoDataModel> dataModels){
+        if(dataModels == null){
+            throw new IllegalArgumentException("DataModel must not be null");
+        }
+        mDataModels = dataModels;
+        mHeights = new ArrayList<>();
+    }
+
+    /**
+     * 创建ViewHolder
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @Override
+    public DemoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //创建item的view
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.photoitem_recycler_view, parent, false);
+        return new DemoViewHolder(itemView);
+    }
+
+    /**
+     * 绑定每一项数据
+     * @param holder
+     * @param position
+     */
+    @Override
+    public void onBindViewHolder(DemoViewHolder holder, int position) {
+         PhotoDataModel dataModel = mDataModels.get(position);
+
+        // 随机高度, 模拟瀑布效果.
+        if (mHeights.size() <= position) {
+            mHeights.add((int) (100 + Math.random() * 300));
+        }
+
+        ViewGroup.LayoutParams lp = holder.getTvLabel().getLayoutParams();
+        lp.height = mHeights.get(position);
+
+        holder.getTvLabel().setLayoutParams(lp);
+
+        holder.getTvLabel().setText(dataModel.getLabel());
+        holder.getTvDateTime().setText(new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH)
+                .format(dataModel.getDateTime()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataModels.size();
+    }
+
+    public void addData(int position) {
+        PhotoDataModel model = new PhotoDataModel();
+        model.setDateTime(getBeforeDay(new Date(), position));
+        model.setLabel("No. " + (int) (new Random().nextDouble() * 20.0f));
+
+        mDataModels.add(position, model);
+        //通知插入--更新
+        notifyItemInserted(position);
+    }
+
+    public void removeData(int position) {
+        mDataModels.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    /**
+     * 获取日期的前一天
+     *
+     * @param date 日期
+     * @param i    偏离
+     * @return 新的日期
+     */
+    private static Date getBeforeDay(Date date, int i) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DAY_OF_YEAR, i * (-1));
+        return calendar.getTime();
+    }
+
+}
--- app/src/main/java/com/photo/PhotosActivity.java
+++ app/src/main/java/com/photo/PhotosActivity.java
@@ -1,11 +1,17 @@
 package com.photo;
 
 import android.graphics.Bitmap;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.widget.ImageView;
 
 import com.BaseActivity;
 import com.kingz.customDemo.R;
 import com.utils.BitMapUtils;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Copyright(C) 2015, 北京视达科科技有限公司
@@ -13,6 +19,19 @@ import com.utils.BitMapUtils;
  * author: King.Z
  * date:  2016/8/6 23:05
  * description:
+ *
+ *   一个采用recycleView的布局页面来显示图片特效（还未完成）.
+ *       LayoutManager: 管理RecyclerView的结构.
+         Adapter: 处理每个Item的显示.
+         ItemDecoration: 添加每个Item的装饰.
+         ItemAnimator: 负责添加\移除\重排序时的动画效果.
+
+        LayoutManager\Adapter是必须, ItemDecoration\ItemAnimator是可选.
+ *
+ *
+ *   tips:8月7号-----打算用GridView来显示。  或者listView  点击后变换图片
+ *
+ *
  */
 public class PhotosActivity extends BaseActivity {
 
@@ -26,10 +45,16 @@ public class PhotosActivity extends BaseActivity {
     private ImageView img5;
     private ImageView img6;
 
+    private RecyclerView recyclerView;
+    private DemoRecyclerAdapter mAdapter;
+
     @Override
     protected void findID() {
         super.findID();
         setContentView(R.layout.photos_activity);
+//        recyclerView = (RecyclerView) findViewById(R.id.test_recycler_view);
+//        initRecyclerView(recyclerView);
+
         img1 = (ImageView) findViewById(R.id.normal_pic);
         img2 = (ImageView) findViewById(R.id.translate_pic);
         img3 = (ImageView) findViewById(R.id.scale_pic);
@@ -38,6 +63,47 @@ public class PhotosActivity extends BaseActivity {
         img6 = (ImageView) findViewById(R.id.circle_pic);
         setImageView();
     }
+
+    /**
+     * 初始化RecyclerView
+     * @param recyclerView
+     */
+    private void initRecyclerView(RecyclerView recyclerView) {
+        recyclerView.setHasFixedSize(true);         // 设置固定大小
+        initRecyclerLayoutManager(recyclerView);    // 初始化布局
+        initRecyclerAdapter(recyclerView);          // 初始化适配器
+        initItemDecoration(recyclerView);           // 初始化装饰
+//        initItemAnimator(recyclerView);           // 初始化动画效果
+    }
+
+    /**
+     * 管理RecyclerView的布局结构
+     * @param recyclerView
+     */
+    private void initRecyclerLayoutManager(RecyclerView recyclerView) {
+        //LayoutManager:GridLayoutManager(网格)/LinearLayoutManager(线性)/StaggeredGridLayoutManager(错列网格)
+        // 错列网格布局 spanCount：方向垂直，就是列数  方向水平，就是行数
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.VERTICAL));
+    }
+
+    /**
+     * 初始化适配器
+     * @param recyclerView
+     */
+    private void initRecyclerAdapter(RecyclerView recyclerView) {
+        //创建初始化数据
+        mAdapter = new DemoRecyclerAdapter(getData());
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    /**
+     * 项的装饰, 比如ListView中的分割线, 在本例中, 左右两条粉线.
+     * @param recyclerView
+     */
+    private void initItemDecoration(RecyclerView recyclerView) {
+        recyclerView.addItemDecoration(new MyItemDecoration(this));
+    }
+
 
     private void setImageView() {
         srcBitmap = BitMapUtils.drawable2Bitmap(getResources().getDrawable(R.drawable.wang), 320, 180);
@@ -74,4 +140,36 @@ public class PhotosActivity extends BaseActivity {
     protected void initIntent() {
         super.initIntent();
     }
+
+    /**
+     * 模拟的数据
+     *
+     * @return 数据
+     */
+    private ArrayList<PhotoDataModel> getData() {
+        int count = 10;
+        ArrayList<PhotoDataModel> data = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            PhotoDataModel model = new PhotoDataModel();
+            model.setDateTime(getBeforeDay(new Date(), i));
+            model.setLabel("No. " + i);
+            data.add(model);
+        }
+        return data;
+    }
+    /**
+     * 获取日期的前一天
+     *
+     * @param date 日期
+     * @param i    偏离
+     * @return 新的日期
+     */
+    private static Date getBeforeDay(Date date, int i) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DAY_OF_YEAR, i * (-1));
+        return calendar.getTime();
+    }
+
+
 }
