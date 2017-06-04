e6b72ed193937e039f89dd7a568f20264d61ca6f
kivensolo <760553478@qq.com>
2017-06-04 22:29:48 +0800

    新增单元测试相关引用

---
+++ app/src/test/java/com/kingz/ExampleUnitTest.java
@@ -0,0 +1,20 @@
+package com.kingz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        int sum = 2 + 2;
+		System.out.println("结果为：" + sum);
+		assertEquals(4, sum);
+    }
+
+}
