a14c3a4f9d95b1330f48de9a9e1c227326a0ed0e
kivensolo <760553478@qq.com>
2017-09-22 19:58:32 +0800

    整理代码

--- app/src/main/java/com/App.java
+++ app/src/main/java/com/App.java
@@ -11,12 +11,9 @@ import com.kingz.utils.ZLog;
 import jp.wasabeef.takt.Takt;
 
 /**
- * Copyright(C) 2015, 北京视达科科技有限公司
- * All rights reserved.
  * author: King.Z
  * date:  2016/5/10 23:25
  */
-//在AndroidManifest.xml中application标签下android:name中指定该类
 @SuppressWarnings("JavaDoc")
 public class App extends Application {
 
