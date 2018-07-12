7fd2b7c46df72eadfcbdcba010c3d51994603c4a
kivensolo-homePC <kiven6969@gmail.com>
2018-07-13 00:08:23 +0800

    title(Gradle Plugin): add gradle plugin demo reason: gradle practice

---
+++ pluginrepo/src/main/groovy/com/kingz/plugin/MyPlugin.groovy
@@ -0,0 +1,10 @@
+package com.kingz.plugin
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class MyPlugin implements Plugin<Project>{
+    @Override
+    void apply(Project project) {
+        println "           [PLUGIN]--->[CustomPlugin apply invoke.. ]"
+    }
+}
