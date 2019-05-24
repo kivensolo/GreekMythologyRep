5e49995b62001bd03f8f697e441adcff15e04206
ZekeWong <760553478@qq.com>
2019-05-24 19:05:23 +0800

    title(update):增加今日头条页面适配代码 reason: 更新

--- app/src/main/java/com/BaseActivity.java
+++ app/src/main/java/com/BaseActivity.java
@@ -2,8 +2,12 @@ package com;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Application;
+import android.content.ComponentCallbacks;
 import android.content.ContentResolver;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.Toast;
 import com.kingz.utils.ZLog;
@@ -82,6 +86,52 @@ public class BaseActivity extends Activity {
             }
             isLoadding = false;
         }
+    }
+
+    private static float mNonCompatDensity;
+    private static float mNonCompatScaleDensity;
+
+    /**
+     * 今日头条屏幕适配方案，以设计图宽360dp的去适配页面
+     * 在onCreate的时候调用
+     * px = dp * density = dp * (dpi/160)
+     * dpi = (sqre(w^2 + h^2) px / 屏幕尺寸 inch ) = 机型不同尺寸
+     *
+     * 1920 * 1080 机子: density == 440
+     * @param act
+     * @param application
+     */
+    private static void setCustomDensity(Activity act, final Application application){
+        DisplayMetrics appDisplayMetrics = application.getResources().getDisplayMetrics();
+        if(mNonCompatDensity == 0){
+            mNonCompatDensity = appDisplayMetrics.density;
+            mNonCompatScaleDensity = appDisplayMetrics.scaledDensity;
+            //监听字体切换
+            application.registerComponentCallbacks(new ComponentCallbacks() {
+                @Override
+                public void onConfigurationChanged(Configuration newConfig) {
+                    if(newConfig != null && newConfig.fontScale > 1){
+                        mNonCompatScaleDensity = application.getResources().getDisplayMetrics().scaledDensity;
+                    }
+                }
+
+                @Override
+                public void onLowMemory() {}
+            });
+        }
+        //以宽度为基准
+        final float targetDensity = appDisplayMetrics.widthPixels / 360;
+        final float targetScaleDensity = targetDensity * (mNonCompatScaleDensity / mNonCompatDensity);
+        final int targetDensityDPI = (int) (160 * targetDensity);
+
+        appDisplayMetrics.density = targetDensity;
+        appDisplayMetrics.scaledDensity = targetScaleDensity;
+        appDisplayMetrics.densityDpi = targetDensityDPI;
+
+        final DisplayMetrics activityDisplayMetries = act.getResources().getDisplayMetrics();
+        activityDisplayMetries.density = targetDensity;
+        activityDisplayMetries.scaledDensity = targetScaleDensity;
+        activityDisplayMetries.densityDpi = targetDensityDPI;
     }
 
 }
