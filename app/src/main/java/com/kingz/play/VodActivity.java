44470ef9bfd315f7a98f4fb22c7feb4e8acee192
ZekeWong <760553478@qq.com>
2019-07-30 19:04:00 +0800

    title(update):调整vodActivity。 reason: 更新

--- app/src/main/java/com/kingz/play/VodActivity.java
+++ app/src/main/java/com/kingz/play/VodActivity.java
@@ -1,8 +1,12 @@
 package com.kingz.play;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
+import com.kingz.customdemo.R;
 import com.kingz.play.fragment.PlayFragment;
 import com.kingz.play.fragment.VodDetailFragment;
 import com.kingz.play.presenter.PlayPresenter;
@@ -19,7 +23,10 @@ public class VodActivity extends PlayerActivity {
     private PlayFragment playFragment;
     private VodDetailFragment vodDetailFragment;
     private PlayPresenter playPresenter;
+    private VodInfoFragment vodInfoFragment;
+    private VodInfoPresenter vodInfoPresenter;
 
+    private VodDetailFragment vodDetailFragment;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +57,42 @@ public class VodActivity extends PlayerActivity {
 //        fragmentTransaction.show(vodInfoFragment);
 //        fragmentTransaction.commit();
     }
+    /**
+     * 展示 收起 详情页
+     *
+     * @param isShow    是否显示
+     * @param videoInfo 详情信息
+     */
+    public void showOrDismissVideoDetail(boolean isShow, VideoInfo videoInfo) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        vodDetailFragment = (VodDetailFragment) fm.findFragmentByTag(TAG_VOD_DETAIL);
+        if (vodDetailFragment == null) {
+            vodDetailFragment = VodDetailFragment.newInstance(videoInfo);
+        }
+        if (isShow) {
+            if (!vodDetailFragment.isAdded()) {
+                fragmentTransaction.add(R.id.content_layout, vodDetailFragment, TAG_VOD_DETAIL);
+            }
+            fragmentTransaction.show(vodDetailFragment);
+        } else {
+            fragmentTransaction.remove(vodDetailFragment);
+        }
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            //目前是横屏的话
+            playFragment.onBackPressed();
+        } else if (vodDetailFragment != null && vodDetailFragment.isAdded() && vodDetailFragment.isVisible()) {
+            getSupportFragmentManager().beginTransaction().remove(vodDetailFragment).commit();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 
 
 }
