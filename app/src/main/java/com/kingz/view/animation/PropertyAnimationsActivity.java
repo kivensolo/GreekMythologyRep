30d85d9fd17427fb8fba60a9e2d7b73573355fcb
ZekeWong <760553478@qq.com>
2020-01-15 16:40:37 +0800

    title(update): delete Copyright .

--- app/src/main/java/com/kingz/view/animation/PropertyAnimationsActivity.java
+++ app/src/main/java/com/kingz/view/animation/PropertyAnimationsActivity.java
@@ -21,8 +21,6 @@ import java.util.List;
 import butterknife.BindView;
 
 /**
- * Copyright(C) 2015, 北京视达科科技有限公司
- * All rights reserved.
  * author: King.Z
  * date:  2016/9/17 20:59
  * description: 属性动画
