44c9a1362d9bef568474b4a63d1e58329474b051
zeke.wang <zeke.wang@xgimi.com>
2020-06-10 17:37:21 +0800

    update(utils):增加颜色值进行透明度处理的方法

--- app/src/main/java/com/kingz/utils/ColorCompatUtils.java
+++ app/src/main/java/com/kingz/utils/ColorCompatUtils.java
@@ -19,4 +19,21 @@ public class ColorCompatUtils {
             return resources.getColor(colorId);
         }
     }
+
+    /**
+     * 将指定颜色进行alpha转换
+     * @param alpha     透明度程度  0~1.0f
+     * @param baseColor 颜色值
+     * @return 新的颜色值
+     */
+    public static int getColorWithAlpha(float alpha, int baseColor) {
+        // ARGB 255,255,255,255   真色彩32位
+        // 透明度占高8位  所以左移24位,确定透明度
+        int a = Math.min(255, Math.max(0, (int) (alpha * 255))) << 24;
+        // ARGB  00000000 11111111 11111111 11111111
+        //      将baseColor转为无透明度的颜色
+        int rgb = 0x00ffffff & baseColor;
+        // A+RGB
+        return a + rgb;
+    }
 }
