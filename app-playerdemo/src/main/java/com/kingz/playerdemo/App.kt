847609e2d1e71ee6ce1e1457f3a262d8fbf9b172
zeke.wang <zeke.wang@xgimi.com>
2020-06-12 15:20:55 +0800

    update(player-deom): 增加Playerdemo模块

---
+++ app-playerdemo/src/main/java/com/kingz/playerdemo/App.kt
@@ -0,0 +1,16 @@
+package com.kingz.playerdemo
+
+import android.app.Application
+
+/**
+ * @author zeke.wang
+ * @date 2020/4/27
+ * @maintainer zeke.wang
+ * @copyright 2020 www.xgimi.com Inc. All rights reserved.
+ * @desc:
+ */
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
