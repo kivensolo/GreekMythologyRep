25878447627743971a62e17ee4ac3efb37f6dfe4
kivensolo-homePC <kiven6969@gmail.com>
2020-07-08 22:52:40 +0800

    title(update): 添加ARouter reason:none

---
+++ module-Common/src/main/java/com/kingz/module/common/router/RouterPath.kt
@@ -0,0 +1,12 @@
+package com.kingz.module.common.router
+
+/**
+ * 公共业务层相关的Router路径
+ */
+interface RouterPath {
+    companion object {
+        const val APP_LAUNCHER = "/app/launcher"
+        const val LOGIN_MAIN = "/login/main"
+        const val LOGIN_SCAN = "/login/scan"
+    }
+}
