2af0bc5c026fc85a1e551dba0d98cdeb3017975c
kivensolo-work <kivensolo@gmail.com>
2021-02-19 15:17:35 +0800

    Title(refactor):删除一些kolin协程demo代码。 Cause：N/A Solution：N/A Effect: N/A Reviewer: N/A 附加说明：N/A

--- app-Kotlin-Coroutines-Examples/src/main/java/com/kingz/coroutines/DemoApp.kt
+++ app-Kotlin-Coroutines-Examples/src/main/java/com/kingz/coroutines/DemoApp.kt
@@ -3,7 +3,6 @@ package com.kingz.coroutines
 import com.kingz.module.common.BuildConfig
 import com.kingz.module.common.CommonApp
 import com.zeke.kangaroo.utils.ZLog
-
 /**
  * WanAndroid:
  * https://github.com/leiyun1993/WanAndroid
