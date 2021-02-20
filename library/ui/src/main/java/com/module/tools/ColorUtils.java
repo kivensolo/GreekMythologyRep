fe1c49f762ab2457fa70d57890cb326ddc485ea7
kivensolo-work <kivensolo@gmail.com>
2021-02-20 17:33:59 +0800

    Title(feat):完善color工具类。 Cause：N/A Solution：N/A Effect: N/A Reviewer: N/A 附加说明：N/A

--- library/ui/src/main/java/com/module/tools/ColorUtils.java
+++ library/ui/src/main/java/com/module/tools/ColorUtils.java
@@ -25,29 +25,40 @@ public class ColorUtils {
 
     /**
      * 将指定颜色进行alpha转换
-     * @param alpha     透明度程度  有效值0~1.0f
+     * @param alpha     颜色的新透明度程度  [0..1]
      * @param srcColor  原始颜色值
-     * @return 带透明度的颜色值
+     * @return 带新透明度的颜色值
      */
-    public static int getColorWithAlpha(float alpha, int srcColor) {
+    public static int replaceColorAlpha(float alpha, int srcColor) {
         // ARGB 255,255,255,255   真色彩32位
-        // 透明度占高8位  所以左移24位,确定透明度
-        int a = Math.min(255, Math.max(0, (int) (alpha * 255))) << 24;
+        // 透明度占高8位  所以左移24 >>>位,确定透明度
+        int a = Math.min(255, Math.max(0, (int) (alpha * 255.0f))) << 24;
         // ARGB  00000000 11111111 11111111 11111111
-        //      将baseColor转为无透明度的颜色
+        //将baseColor转为无透明度的颜色
         int rgb = 0x00ffffff & srcColor;
         // A+RGB
         return a + rgb;
     }
 
     /**
-     * 生成随机颜色
+     * 生成随机颜色(透明度100%)
      * @return 随机颜色 ffxxxxxx
      */
     public static int randomColor() {
         return 0xff000000 |
                 ((int) ((float) (255 * Math.random()) * 255.0f + 0.5f) << 16) |
                 ((int) ((float) (255 * Math.random()) * 255.0f + 0.5f) << 8) |
+                 (int) ((float) (255 * Math.random()) * 255.0f + 0.5f);
+    }
+
+    /**
+     * 生成随机颜色(透明度也随机)
+     * @return 随机的ARGB颜色值
+     */
+    public static int randomAlphaColor() {
+        return ((int) ((float) (255 * Math.random()) * 255.0f + 0.5f) << 24) |
+               ((int) ((float) (255 * Math.random()) * 255.0f + 0.5f) << 16) |
+               ((int) ((float) (255 * Math.random()) * 255.0f + 0.5f) << 8) |
                 (int) ((float) (255 * Math.random()) * 255.0f + 0.5f);
     }
 }
