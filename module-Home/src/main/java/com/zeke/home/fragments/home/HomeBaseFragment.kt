43bed963de349c150ca8c10e3e7ea7efab6f1fa5
kivensolo-work <kivensolo@gmail.com>
2021-04-12 22:17:13 +0800

    Title(ui): 调整fragment_tab的布局。 Cause：调整px为dp,删除tab_cover。 Solution：N/A Effect: N/A。 Reviewer: N/A 附加说明：N/A

--- module-Home/src/main/java/com/zeke/home/fragments/home/HomeBaseFragment.kt
+++ module-Home/src/main/java/com/zeke/home/fragments/home/HomeBaseFragment.kt
@@ -33,19 +33,19 @@ abstract class HomeBaseFragment<T : IPresenter> : BaseFragment(), IView {
     override fun onViewCreated() {
         viewPagerAdapter = HomePagerAdapter(childFragmentManager, PageCreator())
         tableLayout = rootView?.findViewById(R.id.tab_layout)
-        tableLayout!!.setPadding(0, 0, 0, 0)
-        tableLayout!!.visibility = View.GONE
+        tableLayout?.setPadding(0, 0, 0, 0)
+        tableLayout?.visibility = View.GONE
 
-        coverView = rootView?.findViewById(R.id.tab_cover)
+//        coverView = rootView?.findViewById(R.id.tab_cover)
 
         viewPager = rootView?.findViewById(R.id.viewpager)
-        viewPager!!.adapter = viewPagerAdapter
+        viewPager?.adapter = viewPagerAdapter
 
         // tableLayout 与 ViewPager 绑定
-        tableLayout!!.setupWithViewPager(viewPager)
+        tableLayout?.setupWithViewPager(viewPager)
 
         loadStatusView = rootView?.findViewById(R.id.load_status)
-        loadStatusView!!.showProgress()
+        loadStatusView?.showProgress()
     }
 
     override val isShown: Boolean
@@ -58,7 +58,7 @@ abstract class HomeBaseFragment<T : IPresenter> : BaseFragment(), IView {
     override fun hideLoading() {
         loadStatusView?.dismiss()
         tableLayout?.visibility = View.VISIBLE
-        coverView?.visibility = View.VISIBLE
+//        coverView?.visibility = View.VISIBLE
     }
 
     override fun showError() {
