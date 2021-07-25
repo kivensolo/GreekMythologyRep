4be38a842bbf1dfacec5946ec78472cd75737fb1
kivensolo-work <kivensolo@gmail.com>
2021-07-25 17:28:30 +0800

    Title(feat):增加path的demo Cause：N/A Solution：N/A Effect: N/A Reviewer: N/A 附加说明：N/A

---
+++ app/src/main/java/com/kingz/graphics/PathDemoActivity.kt
@@ -0,0 +1,35 @@
+package com.kingz.graphics
+
+import android.os.Bundle
+import android.view.View
+import com.kingz.base.BaseVMActivity
+import com.kingz.base.R
+import com.zeke.reactivehttp.base.BaseReactiveViewModel
+
+/**
+ * author：ZekeWang
+ * date：2021/7/24
+ * description：Path绘制
+ * drawPath(Path path, Paint paint) 画自定义图形
+ * drawPath() 可以绘制自定义图形。
+ * 当你要绘制的图形比较特殊，使用前面的那些方法做不到的时候，就可以使用 drawPath() 来绘制
+ */
+class PathDemoActivity: BaseVMActivity() {
+
+    override val viewModel: BaseReactiveViewModel
+        get() = TODO("Not yet implemented")
+
+    override fun getContentLayout()= R.layout.layout_invalid
+
+    override fun getContentView(): View? {
+        return PathSampleView(baseContext)
+    }
+
+    override fun initView(savedInstanceState: Bundle?) {
+        super.initView(savedInstanceState)
+    }
+
+    override fun initData(savedInstanceState: Bundle?) {
+
+    }
+}
---
+++ app/src/main/java/com/kingz/graphics/PathSampleView.kt
@@ -0,0 +1,71 @@
+package com.kingz.graphics
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.ColorInt
+import com.kingz.customdemo.R
+import com.zeke.kangaroo.utils.UIUtils
+
+class PathSampleView @JvmOverloads constructor(
+    context: Context?, attrs: AttributeSet? = null
+) : View(context, attrs) {
+    var pathPaint: Paint = Paint()
+    var path: Path = Path()
+    var round: Float = 25f
+    @ColorInt
+    var startColor = Color.parseColor("#ffE84749")
+    @ColorInt
+    var endColor = Color.parseColor("#FF00ff00")
+    var bitmap = BitmapFactory.decodeResource(resources, R.drawable.ironman_1)
+    var rectArray = arrayOf(
+        RectF(dp2px(5f), dp2px(5f), dp2px(55f), dp2px(155f)),
+        RectF(dp2px(65f), dp2px(5f), dp2px(115f), dp2px(155f)),
+        RectF(dp2px(125f), dp2px(5f), dp2px(175f), dp2px(155f)),
+        RectF(dp2px(185f), dp2px(5f), dp2px(235f), dp2px(155f)),
+        RectF(dp2px(245f), dp2px(5f), dp2px(295f), dp2px(155f))
+
+//        RectF(dp2px(5f), dp2px(5f), dp2px(55f), dp2px(155f)),
+//        RectF(dp2px(5f), dp2px(165f), dp2px(55f), dp2px(306f)),
+//        RectF(dp2px(5f), dp2px(316f), dp2px(55f), dp2px(456f))
+    )
+
+    private fun dp2px(dpValue: Float): Float {
+        return UIUtils.dip2px(context, dpValue).toFloat()
+    }
+    init {
+        pathPaint.color = startColor
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawColor(Color.WHITE)
+        setShader(null)
+        rectArray.forEach {
+            pathPaint.strokeCap = Paint.Cap.ROUND
+            path.addRoundRect(
+                it,
+                floatArrayOf(
+                    round, round, round, round,
+                    0f, 0f, 0f, 0f
+                ),
+                Path.Direction.CCW
+            )
+            canvas.drawPath(path, pathPaint)
+            path.rewind()
+        }
+    }
+
+    private fun setShader(it: RectF?) {
+        pathPaint.shader = BitmapShader(bitmap,
+            Shader.TileMode.CLAMP,
+            Shader.TileMode.CLAMP
+        )
+//        pathPaint.shader = LinearGradient(
+//            it.left, it.top, it.right, it.bottom,
+//            startColor,
+//            endColor,
+//            Shader.TileMode.CLAMP
+//        )
+    }
+}
