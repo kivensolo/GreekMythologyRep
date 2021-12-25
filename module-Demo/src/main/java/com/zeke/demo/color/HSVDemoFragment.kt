ad9b0ec93fa2917e8b6625064a31ebb1512902bf
kivensolo-work <kivensolo@gmail.com>
2021-12-25 18:37:53 +0800

    Title(feat): 【Demo】HSV的demo调整。 Cause：N/A Solution：N/A Effect: N/A Reviewer: N/A 附加说明：N/A

--- module-Demo/src/main/java/com/zeke/demo/color/HSVDemoFragment.kt
+++ module-Demo/src/main/java/com/zeke/demo/color/HSVDemoFragment.kt
@@ -1,11 +1,14 @@
 package com.zeke.demo.color
 
-import android.graphics.Color
-import android.widget.SeekBar
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.kingz.base.factory.ViewModelFactory
 import com.kingz.module.wanandroid.fragemnts.CommonFragment
 import com.zeke.demo.R
+import com.zeke.demo.databinding.ActivityColorLightBinding
 import kotlinx.android.synthetic.main.activity_color_light.*
 
 /**
@@ -27,9 +30,15 @@ import kotlinx.android.synthetic.main.activity_color_light.*
  */
 class HSVDemoFragment : CommonFragment<ColorViewModel>() {
     private var mColor = 0
-    private var mHue = 0f //色调范围0-360
-    private var mSat = 0f //饱和度范围0-1
-    private var mVal = 0f //亮度范围0-1
+    private lateinit var  colorLightBinding:ActivityColorLightBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        colorLightBinding = ActivityColorLightBinding.inflate(LayoutInflater.from(context))
+        return colorLightBinding.root
+    }
 
     override fun getLayoutResID(): Int = R.layout.activity_color_light
 
@@ -42,7 +51,7 @@ class HSVDemoFragment : CommonFragment<ColorViewModel>() {
         color_picker.apply {
             //颜色初始化
             mColor = color
-            mask_view?.setColor(color)
+            colorLightBinding.maskView.setColor(color)
             setOnColorSelectedListener { mColor = it}
             setOnColorChangedListener {
                 mColor = it
@@ -51,73 +60,10 @@ class HSVDemoFragment : CommonFragment<ColorViewModel>() {
 
             addOpacityBar(opacityBar)
 
-            addSVBar(svBar)
+            addHueBar(hueBar)
             addSaturationBar(saturationBar)
             addValueBar(valueBar)
+            addSVBar(svBar)
         }
-        initSeeks()
     }
-
-
-    //旧版本原生seekBar控制。
-    private fun initSeeks() {
-        //色调
-        hueSeek.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar, progress: Int, b: Boolean) {
-                val hue = (3.6 * progress).toFloat()
-                tv_hue_seek?.text = "$hue"
-                val hsv = FloatArray(3)
-                Color.colorToHSV(mColor, hsv)
-                mHue = hue
-                mSat = hsv[1]
-                mVal = hsv[2]
-                val hsvToColor = Color.HSVToColor(floatArrayOf(mHue, mSat, mVal))
-//                //更新maskView
-//                mask_view.setColor(hsvToColor)
-                //更新颜色选择器
-                color_picker.color = hsvToColor
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar) {}
-            override fun onStopTrackingTouch(seekBar: SeekBar) {}
-        })
-       /* //饱和度
-        satSeek.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                val value = progress.toFloat() / seekBar.max
-                tv_sat_seek?.text = "$value"
-                val hsv = FloatArray(3)
-                Color.colorToHSV(mColor, hsv)
-                mHue = hsv[0]
-                mSat = value
-                mVal = hsv[2]
-                mask_view?.setColor(Color.HSVToColor(floatArrayOf(mHue, mSat, mVal)))
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar) {}
-
-            override fun onStopTrackingTouch(seekBar: SeekBar) {}
-        })
-        //亮度
-        valueSeek.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar, progress: Int, b: Boolean) {
-                val value = progress.toFloat() / seekBar.max
-                tv_value_seek?.text = value.toString()
-                val hsv = FloatArray(3)
-                Color.colorToHSV(mColor, hsv)
-                mHue = hsv[0]
-                mSat = hsv[1]
-                mVal = value
-                if (mVal < 0.35) {
-                    mVal = 0.35f
-                }
-                mask_view?.setColor(Color.HSVToColor(floatArrayOf(mHue, mSat, mVal)))
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar) {}
-            override fun onStopTrackingTouch(seekBar: SeekBar) {}
-        })*/
-
-    }
-
 }
