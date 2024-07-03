11c959b56bac9a45433f0ce413208dbdd3543f52
zekeWong <kivensolo@gmail.com>
2024-07-03 15:50:14 +0800

    Title(style):代码屏蔽。 Cause：N/A Solution：N/A Effect: N/A Reviewer: N/A 附加说明：N/A

--- app/src/main/java/com/kingz/view/webview/core/XWebChromeClient.kt
+++ app/src/main/java/com/kingz/view/webview/core/XWebChromeClient.kt
@@ -1,10 +1,6 @@
 package com.kingz.view.webview.core
 
 import android.net.Uri
-import android.view.View
-import android.webkit.ConsoleMessage
-import android.webkit.JsPromptResult
-import android.webkit.JsResult
 import android.webkit.PermissionRequest
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
@@ -42,46 +38,46 @@ class XWebChromeClient(webHookDispatcher: WebHookDispatcher) : WebChromeClient()
     ): Boolean {
         return mWebHookDispatcher.onShowFileChooser(webView, filePathCallback, fileChooserParams)
     }
-
-    override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
-        mWebHookDispatcher.onConsoleMessage(consoleMessage)
-        return super.onConsoleMessage(consoleMessage)
-    }
-
-    override fun onJsAlert(view: WebView?, url: String?, message: String?, result: JsResult?): Boolean {
-        if (mWebHookDispatcher.onJsAlert(view, url, message, result)) {
-            return true
-        }
-        return super.onJsAlert(view, url, message, result)
-    }
-
-    override fun onJsPrompt(
-        view: WebView?,
-        url: String?,
-        message: String?,
-        defaultValue: String?,
-        result: JsPromptResult?
-    ): Boolean {
-        if (mWebHookDispatcher.onJsPrompt(view, url, message, defaultValue, result)) {
-            return true
-        }
-        return super.onJsPrompt(view, url, message, defaultValue, result)
-    }
-
-    override fun onJsBeforeUnload(view: WebView?, url: String?, message: String?, result: JsResult?): Boolean {
-        if (mWebHookDispatcher.onJsBeforeUnload(view, url, message, result)) {
-            return true
-        }
-        return super.onJsBeforeUnload(view, url, message, result)
-    }
-
-    override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
-        super.onShowCustomView(view, callback)
-        mWebHookDispatcher.onShowCustomView(view, callback)
-    }
-
-    override fun onHideCustomView() {
-        super.onHideCustomView()
-        mWebHookDispatcher.onHideCustomView()
-    }
+//
+//    override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+//        mWebHookDispatcher.onConsoleMessage(consoleMessage)
+//        return super.onConsoleMessage(consoleMessage)
+//    }
+//
+//    override fun onJsAlert(view: WebView?, url: String?, message: String?, result: JsResult?): Boolean {
+//        if (mWebHookDispatcher.onJsAlert(view, url, message, result)) {
+//            return true
+//        }
+//        return super.onJsAlert(view, url, message, result)
+//    }
+//
+//    override fun onJsPrompt(
+//        view: WebView?,
+//        url: String?,
+//        message: String?,
+//        defaultValue: String?,
+//        result: JsPromptResult?
+//    ): Boolean {
+//        if (mWebHookDispatcher.onJsPrompt(view, url, message, defaultValue, result)) {
+//            return true
+//        }
+//        return super.onJsPrompt(view, url, message, defaultValue, result)
+//    }
+//
+//    override fun onJsBeforeUnload(view: WebView?, url: String?, message: String?, result: JsResult?): Boolean {
+//        if (mWebHookDispatcher.onJsBeforeUnload(view, url, message, result)) {
+//            return true
+//        }
+//        return super.onJsBeforeUnload(view, url, message, result)
+//    }
+//
+//    override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
+//        super.onShowCustomView(view, callback)
+//        mWebHookDispatcher.onShowCustomView(view, callback)
+//    }
+//
+//    override fun onHideCustomView() {
+//        super.onHideCustomView()
+//        mWebHookDispatcher.onHideCustomView()
+//    }
 }
